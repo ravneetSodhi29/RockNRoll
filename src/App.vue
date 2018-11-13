bd4f3a23751989b9279913c719a87fb78e94c9cf
Ravneet Sodhi <ravneet.sodhi@nagarro.com>
2018-11-13 12:50:19 +0530

    Add band

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    Welcome to Rock N Roll
+
+    <BandsList> </BandsList>
+  </div>
+</template>
+
+<script>
+import BandsList from '@/components/BandsList.vue'
+
+export default {
+  name: 'app',
+  components: {
+    BandsList
+  }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/BandsList.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <input type="text" placeholder="Add Band.."  @click.stop @keyup.enter="addBand" v-model="newBand">
+    <button @click="addBand"> Add </button>
+
+    <ul>
+      <BandItem v-for="band in bands" :key="band.id" :band="band">
+        {{band.name}}
+      </BandItem>
+    </ul>
+  </div>
+</template>
+
+
+<script>
+import BandItem from "@/components/BandItem.vue";
+
+export default {
+  name: "BandList",
+  components: {
+    BandItem
+  },
+  data() {
+    return {
+      newBand: null,
+      bands: [
+        {
+          id: 1,
+          name: "Black Sabbath",
+          songs: ["Iron Man", "War Pigs"]
+        },
+        {
+          id: 2,
+          name: "ColdPlay",
+          songs: ["Yellow", "Fix you", "Mylo xyloto"]
+        }
+      ]
+    };
+  },
+  methods: {
+    addBand() {
+      if (this.newBand) {
+        var item = { id: this.bands.length + 1, name: this.newBand, songs: [] };
+        this.bands.push(item);
+        this.newBand = "";
+      }
+    }
+  }
+};
+</script>
